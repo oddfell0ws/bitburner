de883eae8d5da3d726bc462b5e39f89d6fdb02e4
Anton C. Swartz IV <djnecrogami@gmail.com>
2022-01-03 11:20:08 -0500

    Adding configs for gui apps

---
+++ gui/config/macro-list.config.js
@@ -0,0 +1,16 @@
+export const macroList = {
+	"Refresh Macros": ["home", "run /gui/macros.js"],
+	"Go home": ["home"],
+	"Show servers": ["home", "run /gui/server-list.js"],
+	"Show Process List": ["home", "run /gui/process-list.js"],
+	"Run XP Farm": ["home", "run /oddfell0ws/xpfarm.js"],
+	"Run Controller": ["home", "run /oddfell0ws/controller.js"],
+	"Run Contracts": ["home", "run /oddfell0ws/contracts.js"],
+	"Run Stats": ["home", "run /oddfell0ws/misc/stats.js"],
+	"Kill Controller": ["home", "kill /oddfell0ws/controller.js"],
+	"Buy All Programs": ["buy BruteSSH.exe", "buy FTPCrack.exe", "buy HTTPWorm.exe", "buy SQLInject.exe", "buy relaySMTP.exe", "buy ServerProfiler.exe", "buy AutoLink.exe", "buy DeepscanV1.exe", "buy DeepscanV2.exe"],
+	"Nuke and Backdoor": ["run BruteSSH.exe", "run FTPCrack.exe", "run HTTPWorm.exe", "run SQLInject.exe", "run relaySMTP.exe", "run NUKE.exe", "backdoor"],
+	"PS List": ["home", "run /oddfell0ws/privateServers.js list"],
+	"PS Analyze": ["home", "run /oddfell0ws/privateServers.js analyze"],
+	"PS Plan": ["home", "run /oddfell0ws/privateServers.js plan"]
+}
---
+++ gui/config/process-list.config.js
@@ -0,0 +1,5 @@
+export const processListPayloads = {
+	grow: ["/oddfell0ws/attack/grow.js"],
+	hack: ["/oddfell0ws/attack/hack.js"],
+	weaken: ["/oddfell0ws/attack/weaken.js"]
+}
